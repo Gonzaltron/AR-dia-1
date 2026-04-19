5b08d4b46d6472fbb7492635eedbaa9c292a6782
Ibai Gonzalez <ibai_gonzalez@euneiz.com>
2026-04-19 11:39:37 +0200

    casa día 1

---
+++ AR dia 1/Assets/Ibai/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.XR.ARFoundation.Samples;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] TMP_Text counterText;
+    [SerializeField] PlaceOnPlane placeOnPlane;
+    [SerializeField] GameObject prefab1;
+    [SerializeField] GameObject prefab2;
+    [SerializeField] Dropdown dropdown;
+    int counter = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        counterText.text = "Numero de planos: " + counter.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void UpdateCounter()
+    {
+        counter++;
+        counterText.text = "Numero de planos: " + counter.ToString();
+    }
+
+    public void ChangePrefab()
+    {
+        if (dropdown.value == 0)
+        {
+            placeOnPlane.placedPrefab = prefab1;
+            Debug.Log("1");
+        }
+        else if (dropdown.value == 1)
+        {
+            placeOnPlane.placedPrefab = prefab2;
+            Debug.Log("2");
+        }
+    }
+}
